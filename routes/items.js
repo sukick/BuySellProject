f846ab27adfd18253d8861283cff98f2c2e10007
sukick <suki.ckchan@gmail.com>
2021-05-02 23:18:40 +0000

    Add route for items

--- routes/items.js
+++ routes/items.js
@@ -17,7 +17,10 @@ router.get('/', (req, res) => {
   itemFunctions.getProducts()
     .then((items) => {
       res.json(items);
-    });
+    })
+    .catch(err => {
+      return console.log("query error", err);
+    })
 });
 
 // GET /products/:id
@@ -25,7 +28,61 @@ router.get('/:id', (req, res) => {
   itemFunctions.getProductById(req.params.id)
     .then((item) => {
       res.json(item);
-    });
+    })
+    .catch(err => {
+      return console.log("query error", err);
+    })
 });
+
+router.post('/', (req, res) => {
+  //res.render("bla bla", {output: req.body});
+
+  console.log("req body:", req.body);
+  const item = {
+    brand: req.body.brand,
+    price: req.body.price,
+    product_number: req.body.product_number,
+    condition: req.body.condition,
+    description: req.body.description
+  };
+  console.log("item:", item );
+  itemFunctions.postItemById(item.brand, item.price, item.product_number, item.condition, item.description)
+  .then((postedItem) => {
+    res.send(postedItem);
+  })
+  .catch(err => {
+    return console.log("query error", err);
+  })
+  // req.context.models.messages[id] = message;
+});
+
+// // POST property
+// router.post('/properties', (req, res) => {
+//   const userId = req.session.userId;
+//   database.addProperty({...req.body, owner_id: userId})
+//     .then(property => {
+//       res.send(property);
+//     })
+// });
+
+// //ADD property
+// const addProperty = function(property) {
+//   const queryString = `
+//   INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+//   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+//   RETURNING *;
+//   `;
+//   const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
+
+//   console.log("values", values)
+//   return pool.query(queryString, values)
+//     .then(res =>
+//      res.rows[0]
+
+//     )
+//     .catch(err => {
+//       return console.log("query error", err);
+//     })
+// }
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,66 @@
 const express = require('express');
 const router = express.Router();
 const userFunctions = require('../lib/users_queries');
+const itemFunctions = require('../lib/items_queries');
+
+// const bodyParser = require('body-parser');
+// app.use(bodyParser);
+
 
 const funcThatReturnsRouter = (db) => {
-  // GET /posts
+  // GET
   router.get('/', (req, res) => {
     userFunctions.getUsers()
       .then((user) => {
         res.json(user);
         // res.render('posts', {posts: response.rows});
-      });
+      })
+      .catch(err => {
+        return console.log("query error", err);
+      })
   });
 
-  // GET /posts/:id
+  // GET /user:id
   router.get('/:id', (req, res) => {
     userFunctions.getUserById(req.params.id)
       .then((user) => {
         res.send(user);
-      });
+      })
+      .catch(err => {
+        return console.log("query error", err);
+      })
   });
+
+  // GET /item:id
+  router.get('/:id', (req, res) => {
+    itemFunctions.getItemById(req.params.id)
+      .then((item) => {
+        res.send(item);
+      })
+        .catch(err => {
+        return console.log("query error", err);
+      })
+  });
+
+  // POST
+
+
+  // DELETE :ID
+//   router.delete('/:messageId', (req, res) => {
+//     const {
+//       [req.params.messageId]: message,
+//       ...otherMessages
+//     } = req.context.models.messages;
+
+//     req.context.models.messages = otherMessages;
+
+//     return res.send(message);
+//   });
 
   return router;
 };
+
+
+
 
 module.exports = funcThatReturnsRouter;

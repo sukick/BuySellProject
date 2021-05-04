5bcd184cfbc5a0a1aad6b908c21319a7cbdc95b7
sukick <suki.ckchan@gmail.com>
2021-05-04 19:32:40 +0000

    Add nav

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(morgan('dev'));
 app.use(cookiesession({ name: 'session', keys:['key1']}));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const itemsRouter = require('./routes/items');
 const usersRouter = require('./routes/users');

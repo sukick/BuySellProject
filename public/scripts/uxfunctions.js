0fbbb3b2cb0b542cabf41440f9c7135e9beb0627
Rashad Bayram <rashadabayram.com>
2021-05-05 21:00:09 -0600

    updated git ignore

--- public/scripts/uxfunctions.js
+++ public/scripts/uxfunctions.js
@@ -16,10 +16,11 @@ $(function() {
 
 $(function() {
   let products = $( "#hiddendata" ).val();
-  products = JSON.parse(products);
+  //products = JSON.parse(products);
+  products = JSON.parse(JSON.stringify(products));
   const min = $( "#min" ).val();
   const max = $( "#max" ).val();
-  console.log(products);
+  console.log(typeof products);
   //const filtered =
 
   $(".filter").on('click', function(event) {

a9e5c8952a21d0d3ecb21794d750914f73770122
Harp Sandhu <happacode@protonmail.com>
2021-05-06 21:47:23 -0700

    Add styling and item images

--- db/seeds/02_items_info.sql
+++ db/seeds/02_items_info.sql
@@ -1,5 +1,9 @@
-INSERT INTO items (brand, price, product_number, condition, image, description) VALUES ('Fender', 300, 13579, 'New','/img/fender-pp-bass.jpeg','Nice guitar');
-INSERT INTO items (brand, price, product_number, condition, image, description) VALUES ('Yamaha', 500, 97531, 'New','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8-Sr3hipeKmvk3OgJy5GeK-XL-IVaBSKf75A3XSJxaR-riI1p7D6kWRme7Q&usqp=CAc', 'Great guitar');
-INSERT INTO items (brand, price, product_number, condition, image, description, sold) VALUES ('Gibson', 700, 35719, 'New','https://images.reverb.com/image/upload/s--v2B1oNKk--/f_auto,t_large/v1617157114/hqsvi1u0wkcbjebwef9m.jpg', 'Cool guitar', true);
+INSERT INTO items (brand, price, product_number, condition, image, description) VALUES ('Fender', 900, 13579, 'New','/img/fender-pp-bass.jpeg','Nice guitar');
+INSERT INTO items (brand, price, product_number, condition, image, description) VALUES ('Yamaha', 500, 97531, 'Used','/img/yamaha-jr1.jpeg', 'Perfect starter acoustic guitar');
+INSERT INTO items (brand, price, product_number, condition, image, description, sold) VALUES ('Gibson', 700, 35390, 'New','https://images.reverb.com/image/upload/s--v2B1oNKk--/f_auto,t_large/v1617157114/hqsvi1u0wkcbjebwef9m.jpg', 'Amazing guitar', true);
+INSERT INTO items (brand, price, product_number, condition, image, description) VALUES ('Squier', 1300, 11579, 'New','/img/Squier-Stratocaster.jpeg','Smoothest sound around');
+INSERT INTO items (brand, price, product_number, condition, image, description) VALUES ('Taylor', 800, 67531, 'New','/img/taylor-gs-mini.jpeg', 'Cool bass guitar');
+INSERT INTO items (brand, price, product_number, condition, image, description, sold) VALUES ('Martin', 700, 35719, 'Used','/img/martin_lx1.jpeg', 'You will love playing this beauty', true);
+INSERT INTO items (brand, price, product_number, condition, image, description) VALUES ('Rickenbacker', 1500, 45531, 'New','/img/rickenbacker-660.jpeg', 'Simply the best feeling');
 
 
